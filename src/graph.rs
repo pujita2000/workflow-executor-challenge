f6a8fb4e541dd6284ada18c31c2e4be3ce173ed0
pujita2000 <93445438+pujita2000@users.noreply.github.com>
2026-01-18 23:02:43 -0500

    Check for disconnected graphs (#4)

--- src/graph.rs
+++ src/graph.rs
@@ -6,7 +6,6 @@ use std::collections::{HashMap, HashSet};
 use crate::{parse_input_reference, workflow::Workflow};
 
 pub type NodeId = String;
-pub type AdjList = HashMap<NodeId, Vec<NodeId>>;
 
 // TODO (PS): add a phantom marker to tie lifetime to workflow?
 /// Key components of Workflow DAG, tracks state changes and job ouputs
